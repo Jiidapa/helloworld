494097d2dd45665a3f0c04836f505ad1ca4c96bc
jiidapa <jidapa.p@kkumail.com>
2019-08-30 16:46:05 +0700

    Updates

---
+++ precache-manifest.a2d4bdd6b93e5eaf0e42da97c9e8d922.js
@@ -0,0 +1,70 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ba8a7bff245b80c6dc0f377dcb879ec7",
+    "url": "/helloworld/index.html"
+  },
+  {
+    "revision": "ed370e99ea86b52e924f",
+    "url": "/helloworld/static/css/2.22a7d4ef.chunk.css"
+  },
+  {
+    "revision": "80e606a78651d2b35968",
+    "url": "/helloworld/static/css/main.30086e5c.chunk.css"
+  },
+  {
+    "revision": "ed370e99ea86b52e924f",
+    "url": "/helloworld/static/js/2.83fddeb8.chunk.js"
+  },
+  {
+    "revision": "80e606a78651d2b35968",
+    "url": "/helloworld/static/js/main.0650cffd.chunk.js"
+  },
+  {
+    "revision": "e1015fa242fcb07a87f7",
+    "url": "/helloworld/static/js/runtime~main.97614c2e.js"
+  },
+  {
+    "revision": "45e50cd0f6a5f26e86943e95e35c1e29",
+    "url": "/helloworld/static/media/easy1.45e50cd0.jpg"
+  },
+  {
+    "revision": "618b65d80273f053d3c9cf16d7b4c56d",
+    "url": "/helloworld/static/media/easy2.618b65d8.jpg"
+  },
+  {
+    "revision": "06660c3751759bec3b08b42a40bc0931",
+    "url": "/helloworld/static/media/easy3.06660c37.jpg"
+  },
+  {
+    "revision": "474f86f0dffe390a5a0b7f61fe2fd7f1",
+    "url": "/helloworld/static/media/easy4.474f86f0.jpg"
+  },
+  {
+    "revision": "cbafa58ce396bdc247f004daf6ad136a",
+    "url": "/helloworld/static/media/jida.cbafa58c.jpg"
+  },
+  {
+    "revision": "2caca8c6254b61ecc1a4c05993b97f1c",
+    "url": "/helloworld/static/media/krixxi1.2caca8c6.jpg"
+  },
+  {
+    "revision": "81e73a1c4a2d84e93f347925028a9a87",
+    "url": "/helloworld/static/media/krixxi2.81e73a1c.jpg"
+  },
+  {
+    "revision": "ff06e185bcee283ebb9430b58ddc303a",
+    "url": "/helloworld/static/media/logo.ff06e185.png"
+  },
+  {
+    "revision": "aed3c0b8098ec49a9e9d16e2b7a33be4",
+    "url": "/helloworld/static/media/pig1.aed3c0b8.jpg"
+  },
+  {
+    "revision": "0eb2da6747a9680d408c7c8496bda9d5",
+    "url": "/helloworld/static/media/pig2.0eb2da67.JPG"
+  },
+  {
+    "revision": "a44523eaa06c604fd3630eacb4bf87cf",
+    "url": "/helloworld/static/media/pig3.a44523ea.jpg"
+  }
+]);
